478cf45fe9dae46f10624c423ec4da93f1173d66
Stephen Crane <scrane88@gmail.com>
2021-08-05 09:39:04 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  output = ""
+  case array.length
+  when 1 then output = array[0]
+  when 2 then output = array.join(" and ")
+  else
+    output_array = array
+    output_array[-1] = "and " + output_array[-1]
+    output = output_array.join(", ")
+  end
+  output
 end
